b96d5e34ab9e6c228ab7b7b65667e43ec69a7c55
John Doe <anjalisony32@gmail.com>
2021-09-07 17:34:23 +0530

    clean repo

--- lib/__init__.py
+++
@@ -1,9 +0,0 @@
-from lib.service import (  # noqa: E402
-    BentoService,
-    api_decorator as api,
-    env_decorator as env,
-    web_static_content_decorator as web_static_content,
-    artifacts_decorator as artifacts,
-    ver_decorator as ver,
-    save,
-)
